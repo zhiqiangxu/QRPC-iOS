df10288da1bfe0b07e1ef5b66aac930aeee3f25f
jiguanhu <jiguanhu@innotechx.com>
2019-12-11 13:52:38 +0800

    Initial commit

---
+++ Example/InnotechIMProtocol/INXViewController.h
@@ -0,0 +1,13 @@
+//
+//  INXViewController.h
+//  InnotechIMProtocol
+//
+//  Created by jiguanhu on 12/11/2019.
+//  Copyright (c) 2019 jiguanhu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface INXViewController : UIViewController
+
+@end
